509df98c48b4a39c34e68cc8e01d3446fb9b7ee9
Tania <tatiana_mititelu@yahoo.com>
2025-04-09 08:13:12 +0000

    Enable Django login and logout system

--- swapspot/urls.py
+++ swapspot/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('listings.urls')),  
+    path('', include('listings.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),  # ✅ Enables login/logout
 ]
